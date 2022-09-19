1b3f338206b5467daf441447ce641af03feb0a60
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-19 15:06:45 -0700

    Add selection to object and array to object

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const gameBoard = {
+    game: [{topLeft: "", topMid: "", topBottom: "", midTop: "", midMid: "", midBottom: "", rightTop: "", rightMid: "", rightBottom: ""}],
+    player1: "X",
+    player2: "O"
+
+}
+
 
 const grid = document.querySelectorAll('.game');
 
@@ -7,7 +14,8 @@ grid.forEach(square => {
 
 function addMove(e){
     if(e.target.textContent === ""){
-        e.target.textContent = 'X'
+        e.target.textContent = gameBoard.player1
+        gameBoard.game.push()
     }else{
         return
     }
