e4b56e54a17ff3676ce6c276bc79011b8582d994
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-19 13:04:13 -0700

    Add ability to add item to dom

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+const grid = document.querySelectorAll('.game');
+
+grid.forEach(square => {
+    square.addEventListener('click', addMove)
+})
+
+function addMove(e){
+    if(e.target.textContent === ""){
+        e.target.textContent = 'X'
+    }else{
+        return
+    }
+}
+
