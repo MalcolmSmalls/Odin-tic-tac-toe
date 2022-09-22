d8dc291951c735f03559f68a2304aeb91b3fd3f3
Malcolm Smalls <bruhbigchop@gmail.com>
2022-09-21 17:55:52 -0700

    Add conditionals to determine winner

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const gameBoard = {
+    game: [],
+    player1: "X",
+    player2: "O"
+
+}
+
+
+const grid = document.querySelectorAll('.game');
+
+grid.forEach(square => {
+    square.addEventListener('click', addMove)
+})
+
+function addMove(e){
+    let move = e.target.classList[0]
+    let makeMove = gameBoard.game
+    if(e.target.textContent === ""){
+        e.target.textContent = gameBoard.player1
+        gameBoard.game.push({move})
+    }else{
+        return
+    }
+    for (const property in makeMove){
+        console.log(makeMove[property].move === 'leftMid')
+    }
+}
+
