9e78fc54edf23974a430c46b676f52a13830c722
MalcolmSmalls <smallsbeats@gmail.com>
2022-09-21 21:05:19 -0700

    Add win combination object

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const gameBoard = {
-    game: [],
+    game: [{
+        winning1: ['leftTop', 'midTop', 'rightTop'],
+        winning2: ['leftTop', 'midMid', 'rightBottom'],
+        winning3: ['leftTop', 'leftMid', 'leftBottom'],
+        winning4: []
+    }],
     player1: "X",
     player2: "O"
 
@@ -14,18 +19,38 @@ grid.forEach(square => {
 
 function addMove(e){
     let move = e.target.classList[0]
-    let makeMove = gameBoard.game
+
     if(e.target.textContent === ""){
         e.target.textContent = gameBoard.player1
         gameBoard.game.push({move})
     }else{
         return
     }
-    for (const property in makeMove){
-        console.log(makeMove.includes('leftMid'))
-        // if(makeMove[property].move === 'leftMid' && makeMove[property].move === 'midMid' && makeMove[property].move === 'rightMid'){
-        //     alert('You Win!')
-        // }
-    }
+
+    scoreBoard()
+
+    // makeMove.forEach(item =>{
+    //     console.log(item)
+    // })
+
+
+    // for (const property in makeMove){
+
+    //     if(makeMove[property].move === 'leftMid' && makeMove[property].move === 'midMid' && makeMove[property].move === 'rightMid'){
+    //         alert('You Win!')
+    //     }
+    // }
 }
 
+const scoreBoard = () => {
+    let score = 0
+    let makeMove = gameBoard.game
+        for (const property in makeMove){
+            console.log(makeMove[property].move)
+
+        if(makeMove[property].move === 'leftMid' && makeMove[property].move === 'midMid' && makeMove[property].move === 'rightMid'){
+            alert('You Win!')
+        }
+    }
+
+}
